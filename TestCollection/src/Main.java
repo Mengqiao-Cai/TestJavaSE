59b7c01dd48e642d7dc200452f3a3b52a0cee0c6
Mengqiao-Cai <mengqiao@bu.edu>
2024-08-02 20:33:38 -0600

    Collection tests

---
+++ TestCollection/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author: cmq
+ * @date: 8/2/2024 - 08 - 02 - 8:00 PM
+ * @version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
